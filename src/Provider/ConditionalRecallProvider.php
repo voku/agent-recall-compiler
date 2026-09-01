40342a57d500c354c997d0538a065e7771e7afd8
Lars Moelleken <lars@moelleken.org>
2026-09-01 06:17:19 +0200

    fix(recall): bind optional providers to current root

--- src/Provider/ConditionalRecallProvider.php
+++ src/Provider/ConditionalRecallProvider.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace voku\AgentRecallCompiler\Provider;
 
+use voku\AgentRecallCompiler\RecallRootConfig;
+
 interface ConditionalRecallProvider extends RecallProvider
 {
-    public function isAvailable(): bool;
+    public function isAvailable(RecallRootConfig $rootConfig): bool;
 }
